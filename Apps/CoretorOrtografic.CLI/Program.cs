8d4ff7e49bbc338bf37e572672e1220cc2e851a4
Massimo Romanin <dev@massimoromanin.com>
2025-07-01 15:49:48 +0200

    feat: load CLI messages from JSON

--- Apps/CoretorOrtografic.CLI/Program.cs
+++ Apps/CoretorOrtografic.CLI/Program.cs
@@ -1,9 +1,8 @@
-﻿using CoretorOrtografic.Infrastructure.SpellChecker;
+using CoretorOrtografic.Infrastructure.SpellChecker;
 using CoretorOrtografic.Core.Input;
 using CoretorOrtografic.Core.SpellChecker;
 using Autofac;
 using System;
-using System.Threading;
 using System.Linq;
 using System.Collections.Generic;
 using Components.CoretorOrtografic.Entities.ProcessedElements;
@@ -18,52 +17,49 @@ namespace CoretorOrtografic.CLI
 
         public static void Main(string[] args)
         {
-
             _container = CoretorOrtograficCliDependencyContainer.Configure
 #if DEBUG
                 (true);
 #else
                 (false);
 #endif
-
             Console.ForegroundColor = ConsoleColor.White;
 
-            PrintTitle();
+            Localization.SelectLanguage();
+            AsciiLogo.Print();
             PrintInstructions();
 
             using (var scope = _container.BeginLifetimeScope())
             {
                 _reader = scope.Resolve<IContentReader>();
-
                 _checker = scope.Resolve<ISpellChecker>();
 
                 while (true)
                 {
                     var readStrings = _reader.Read()?.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
-
                     if (readStrings is null || !readStrings.Any())
                     {
-                        Console.WriteLine("No commands or words were provided.");
+                        Console.WriteLine(Localization.GetNoWordsProvided());
                         PrintInstructions();
                     }
-                    else if (readStrings.First().ToUpper() == "Q" || readStrings.First().ToUpper() == "QUIT")
+                    else if (readStrings.First().Equals("Q", StringComparison.OrdinalIgnoreCase) || readStrings.First().Equals("QUIT", StringComparison.OrdinalIgnoreCase))
                     {
-                        Console.WriteLine("Closing application...");
+                        Console.WriteLine(Localization.GetClosing());
                         Environment.Exit(1);
                     }
                     else if (readStrings.Count == 1)
                     {
-                        Console.WriteLine("Please provide a command and at least a word to check.");
+                        Console.WriteLine(Localization.GetProvideCommandAndWord());
                         PrintInstructions();
                     }
                     else if (readStrings.First().Length != 1)
                     {
-                        Console.WriteLine($"Unknown command '{readStrings.First()}'.");
+                        Console.WriteLine(Localization.GetUnknownCommand(readStrings.First()));
                         PrintInstructions();
                     }
                     else
                     {
-                        switch (Char.ToUpper(readStrings.First().Single()))
+                        switch (char.ToUpperInvariant(readStrings.First().Single()))
                         {
                             case 'C':
                                 PrintWordsCorrectness(readStrings.Skip(1).ToList());
@@ -72,7 +68,7 @@ namespace CoretorOrtografic.CLI
                                 PrintSuggestedWords(readStrings.Skip(1).ToList());
                                 break;
                             default:
-                                Console.WriteLine($"Unknown command '{readStrings.First()}'.");
+                                Console.WriteLine(Localization.GetUnknownCommand(readStrings.First()));
                                 PrintInstructions();
                                 break;
                         }
@@ -81,98 +77,34 @@ namespace CoretorOrtografic.CLI
             }
         }
 
-        public static void PrintTitle()
+        private static void PrintInstructions()
         {
-            Console.Title = "ASCII Art";
-            string title = @"
-                                                                                      
-                                                ******************                                   
-                                            ************************+++                             
-                                          *************************+++++++++*                       
-                                        *************************++++++++++++++++       +=+=+       
-                                ===~~~~+***********************+++++++++++++++++++++++++++==+       
-                           +=======~~~~=+********************+++++++++++++++++++++++++++=====       
-                        +++========~~~~~+******************+++++++++++++++++++++++++++======        
-                      ++++++=======~~~~~=****************++++++++++++++++++++++++++++=======        
-                    ++++++++=======~~~~~~+**************+++++++++++++++++++++++++++========         
-                  +*++++++++=======~~~~~~=*************+++++++++++++++++++++++++++========          
-                  **++++++++=======~~~~~~~+**********+++++++++++++++++++++++++++========            
-                 ***++++++++=======~~~~~~~~+*******+++++++++++++++++++++++++++========              
-                 ***++++++++=======~~~~~~~~-+****+++++++++++++++++++++++++++==========              
-       >>><      ***++++++++=======~~~~~~~~--=++++++++++++++++++++++++++++============              
-        >><<<<<             (((((()<>+~~~~~---~+++++++++++++++++++++++++==============              
-         ><<<<<<<<<<<))))))))((((((((((<>=~-----=++++++++++++++++++++++================             
-          <<<<<<<<<<)))))))))((((((((((]]](>=----~=++++++++++++++++++==================             
-           <<<<<<<<<))))))))))<>>^^*+==~~~~~-------~=++++++++++++++=====================            
-              <<<<<<)<<>>^++=======~~~~~~~~-------:::-=++++++++++=======================            
-                      ++++++=======~~~~~~~~-----~~=++++++++++++=========================            
-                         ++========~~~~~~~~--~+++++++++++++++==========================~=           
-                              +====~~~~~      +++++++++++++++++======================~~~~           
-                                              +=+++++++++++++++++==================~~~~~~           
-                                              ==+++++++++++++++++++===============~~~~==+^          
-                                              ====+++++++++++++++++++===========~~==*>))<<          
-                                              ======++++++++++++++++++=======~==*<)))<<<<<          
-                                              ========++++++++++++++++++=====>))))<<<<<<<<>         
-                                              ==========++++++++++++++++**<)))<<<<<<<<<<>>>         
-                                              ===========+++++++++++++^<)))<<<<<<<<<<>>>>>>         
-                                              =============++++++++*><<<<<<<<<<<<>>>>>>>>>>         
-                                             ================++++^<<<<<<<<<<<<>>>>>>>>>             
-                                             =~==============+*><<<<<<<<<<>>>>>>>>>>                
-                                             ~~~~==========+^<<<<<<<<<<>>>>>>>>>>                   
-                                             ~~~~~=======+><<<<<<<<>>>>>>>>>>>                      
-                                            ~~~~~~~~===*><<<<<<<>>>>>>>>>>>                         
-                                            ~~~~~~~~=*><<<<<>>>>>>>>>>>>                            
-                                           ~~~~~~~=*><<<>>>>>>>>>>>>>                               
-                  ]]]]                    -~~~~~=*><<>>>>>>>>>>>>>>                                 
-               ]]]]]]]]                  ~-~~~=*>>>>>>>>>>>>>>>>                                    
-             ]]]]]]]](((                ----~*>>>>>>>>>>>>>>^>                                      
-             (]]]((((((((               --~*>>>>>>>>>>>>>>^                                         
-              (((((()))))))           ---+>>>>>>>>>>>^^^>                                           
-               (())))))))<<<         --=^>>>>>>>>^^^^^>                                             
-               )))))))<<<<<<<>      -=^>>>>>>^^^^^^^^                                               
-                ))<<<<<<<<<<>>><   =*>>>>>^^^^^^^^^                                                 
-                 <<<<<<<>>>>>>>>>^^>>>^^^^^^^^^^^                                                   
-                  <<<>>>>>>>>>>>>>>^^^^^^^^^^^^                                                     
-                    >>>>>>>>>>>^^^^^^^^^^^^^^                                                       
-                     >>>>>>>^^^^^^^^^^^^^^^                                                         
-                      >>^^^^^^^^^^^^^^^^^^                                                          
-                        ^^^^^^^^^^^^^^^^                                                            
-                         ^^^^^^^^^^^^^^                                                             
-                           ^^^^^^^^^^                                                               
-                            ^^^^^^^                                                                 
-                              ^^^^                                                                                  
-                                                                                                    
-        ";
-
-            Console.WriteLine(title);
+            Console.WriteLine(Localization.GetInstructions());
         }
-        public static void PrintInstructions()
-        {
 
-        }
         private static void PrintWordsCorrectness(List<string> words)
         {
             try
             {
-                _checker.ExecuteSpellCheck(String.Join(" ", words));
+                _checker.ExecuteSpellCheck(string.Join(" ", words));
 
                 foreach (ProcessedWord processedWord in _checker.ProcessedWords)
                 {
                     Console.ForegroundColor = ConsoleColor.Blue;
                     Console.Write($"{processedWord.Original}");
                     Console.ForegroundColor = ConsoleColor.White;
-                    Console.Write(" is ");
+                    Console.Write($" {Localization.GetIs()} ");
                     if (processedWord.Correct)
                     {
                         Console.ForegroundColor = ConsoleColor.Green;
-                        Console.Write("correct");
+                        Console.Write(Localization.GetCorrect());
                         Console.ForegroundColor = ConsoleColor.White;
                         Console.WriteLine(".");
                     }
                     else
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
-                        Console.Write("incorrect");
+                        Console.Write(Localization.GetIncorrect());
                         Console.ForegroundColor = ConsoleColor.White;
                         Console.WriteLine(".");
                     }
@@ -188,39 +120,40 @@ namespace CoretorOrtografic.CLI
                 _checker.CleanSpellChecker();
             }
         }
+
         private static void PrintSuggestedWords(List<string> words)
         {
             try
             {
-                _checker.ExecuteSpellCheck(String.Join(" ", words));
+                _checker.ExecuteSpellCheck(string.Join(" ", words));
 
                 foreach (ProcessedWord processedWord in _checker.ProcessedWords)
                 {
                     Console.ForegroundColor = ConsoleColor.Blue;
                     Console.Write($"{processedWord.Original}");
                     Console.ForegroundColor = ConsoleColor.White;
-                    Console.Write(" is ");
+                    Console.Write($" {Localization.GetIs()} ");
                     if (processedWord.Correct)
                     {
                         Console.ForegroundColor = ConsoleColor.Green;
-                        Console.Write("correct");
+                        Console.Write(Localization.GetCorrect());
                         Console.ForegroundColor = ConsoleColor.White;
                         Console.WriteLine(".");
                     }
                     else
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
-                        Console.Write("incorrect");
+                        Console.Write(Localization.GetIncorrect());
                         Console.ForegroundColor = ConsoleColor.White;
                         Console.Write(". ");
                         var suggestedWords = _checker.GetWordSuggestions(processedWord).Result;
                         if (suggestedWords is null || !suggestedWords.Any())
                         {
-                            Console.WriteLine("There are no suggestions.");
+                            Console.WriteLine(Localization.GetNoSuggestions());
                         }
                         else
                         {
-                            Console.Write("Suggestions are: ");
+                            Console.Write(Localization.GetSuggestionsAre());
                             foreach (var suggestedWord in suggestedWords)
                             {
                                 Console.ForegroundColor = ConsoleColor.Yellow;

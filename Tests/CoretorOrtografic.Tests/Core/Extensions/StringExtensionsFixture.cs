b436da19588433d285df4fc6a0c6d89b1151f544
Massimo Romanin <dev@massimoromanin.com>
2025-07-08 17:42:50 +0200

    feat(tests): implement tests for StringExtensions

---
+++ Tests/CoretorOrtografic.Tests/Core/Extensions/StringExtensionsFixture.cs
@@ -0,0 +1,36 @@
+﻿using CoretorOrtografic.Core.Extensions;
+using NUnit.Framework;
+
+namespace CoretorOrtografic.Tests.Core
+{
+    public class StringExtensionsFixture
+    {
+        [Test]
+        public void ToFirstCharacterUpper_RegularWord_ReturnsCapitalized()
+        {
+            var result = "cjase".ToFirstCharacterUpper();
+            Assert.That(result, Is.EqualTo("Cjase"));
+        }
+
+        [Test]
+        public void ToFirstCharacterUpper_WordStartingWithApostrophe_ReturnsCapitalized()
+        {
+            var result = "'cjase".ToFirstCharacterUpper();
+            Assert.That(result, Is.EqualTo("'Cjase"));
+        }
+
+        [Test]
+        public void ToFirstCharacterUpper_SingleApostrophe_ReturnsUnchanged()
+        {
+            var result = "'".ToFirstCharacterUpper();
+            Assert.That(result, Is.EqualTo("'"));
+        }
+
+        [Test]
+        public void ToFirstCharacterUpper_NullOrEmpty_ReturnsOriginal()
+        {
+            Assert.That(((string)null).ToFirstCharacterUpper(), Is.Null);
+            Assert.That("".ToFirstCharacterUpper(), Is.EqualTo(""));
+        }
+    }
+}
